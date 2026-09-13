10c277acb5837bd68eec60db4a85d5a43de48f0a
Roman Akinfeev <akinfold@gmail.com>
2026-09-13 05:15:25 +0300

    Modernize packaging and CI for 0.2.1

--- src/requests_unifi_auth/__init__.py
+++ src/requests_unifi_auth/__init__.py
@@ -2,7 +2,7 @@ from .auth import UnifiControllerAuth as UnifiControllerAuth
 from .auth import UnsafeRedirectError as UnsafeRedirectError
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "__version__",
     "UnifiControllerAuth",
